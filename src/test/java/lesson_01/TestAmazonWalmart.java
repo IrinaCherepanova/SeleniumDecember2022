0deefd2a796e2641018310066df63dfe3dfc3110
IrinaCherepanova <cherepanovaira@gmail.com>
2023-02-07 21:18:23 +0300

    hw

--- src/test/java/lesson_01/TestAmazonWalmart.java
+++ src/test/java/lesson_01/TestAmazonWalmart.java
@@ -4,39 +4,25 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class MyFirstRun {
+public class TestAmazonWalmart {
+    public static void main(String[] args) {
 
-    public static void main(String[] args) throws InterruptedException {
-
-        // Setup chromedriver
         WebDriverManager.chromedriver().setup();
 
-        // Initialization of ChromeDriver
         WebDriver driver = new ChromeDriver();
 
-        driver.manage().window().maximize();
+        driver.get("https://www.amazon.com/");
 
-        driver.get("https://www.google.com");
-
-
-        // Current URL
         System.out.println("driver.getCurrentUrl() = " + driver.getCurrentUrl());
-
-        //System.out.println("driver.getPageSource() = " + driver.getPageSource());
-
         System.out.println("driver.getTitle() = " + driver.getTitle());
 
-        driver.navigate().to("https://www.yandex.ru");
-
+        driver.navigate().to("https://www.walmart.com/");
         driver.navigate().back();
-
         driver.navigate().forward();
-
         driver.navigate().refresh();
 
+        System.out.println("driver.getCurrentUrl() = " + driver.getCurrentUrl());
         System.out.println("driver.getTitle() = " + driver.getTitle());
-
-        Thread.sleep(3000);
 
         driver.quit();
 
